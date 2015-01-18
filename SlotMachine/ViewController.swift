5336dc62ea43ef184bdc6d1fc79d9d78e396ea2a
Charlie Franklin <charlie.franklin@me.com>
2015-01-19 06:48:21 +1100

    first display set up

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -15,10 +15,15 @@ class ViewController: UIViewController {
     var thirdContainer: UIView!
     var fourthContainer: UIView!
     
+    let kMarginForView:CGFloat = 10.0
+    let kSixth:CGFloat = 1.0/6/0
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupContainerViews()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,6 +31,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func setupContainerViews () {
+        self.firstContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: self.view.bounds.origin.y, width: self.view.bounds.width - (kMarginForView * 2), height: self.view.bounds.height * kSixth))
+        self.firstContainer.backgroundColor = UIColor.redColor()
+        self.view.addSubview(self.firstContainer)
+        
+    }
+    
+    
+    
 
 }
 
